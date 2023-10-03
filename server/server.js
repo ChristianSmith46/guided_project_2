5943dc03dc1b07787181f0a972a5429a20218fc3
Christian Smith <christian.46.smith@gmail.com>
2023-10-03 13:47:18 -0400

    Complete swapi routes other than /api/planets/:id/characters

--- server/server.js
+++ server/server.js
@@ -1,62 +1,277 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const dbName = 'swapi';
 
 const port = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/api/characters', (req, res) => {
-
+app.get('/api/characters', async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('characters');
+        const charactersArr = await collection.find({}).toArray();
+        res.json(charactersArr);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
-app.get('/api/films', (req, res) => {
-    
+app.get('/api/films', async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('films');
+        const filmsArr = await collection.find({}).toArray();
+        res.json(filmsArr);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
-app.get('/api/planets', (req, res) => {
-    
+app.get('/api/planets', async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('planets');
+        const planetsArr = await collection.find({}).toArray();
+        res.json(planetsArr);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
-app.get('/api/characters/:id', (req, res) => {
-    const id = req.params.id;
+app.get('/api/characters/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('characters');
+        const character = await collection.findOne({ id });
+        if (!character) res.json({ error: "No Character found" })
+        else res.json(character);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
-app.get('/api/films/:id', (req, res) => {
-    const id = req.params.id;
-    
+app.get('/api/films/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('films');
+        const film = await collection.findOne({ id });
+        if (!film) res.json({ error: "No film found" })
+        else res.json(film);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
-app.get('/api/planets/:id', (req, res) => {
-    const id = req.params.id;
-    
+app.get('/api/planets/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('planets');
+        const planet = await collection.findOne({ id });
+        if (!planet) res.json({ error: "No planet found" })
+        else res.json(planet);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
-app.get('/api/films/:id/characters', (req, res) => {
-    const id = req.params.id;
-    
+app.get('/api/films/:id/characters', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('films');
+        const filmData = await collection.aggregate([
+            { $match: { id } },
+            {
+                $lookup: {
+                    from: 'films_characters',
+                    localField: 'id',
+                    foreignField: 'film_id',
+                    as: 'characterList',
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: 'characters',
+                                localField: 'character_id',
+                                foreignField: 'id',
+                                as: 'character'
+                            }
+                        },
+                        {
+                            $unwind: "$character"
+                        }
+                    ]
+                }
+            }
+        ]).toArray();
+
+        const characters = filmData[0]?.characterList.map((item) => {
+            return item.character;
+        });
+
+        if (!characters) res.json({ error: "No characters found" })
+        else res.json(characters);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
-app.get('/api/films/:id/planets', (req, res) => {
-    const id = req.params.id;
-    
+app.get('/api/films/:id/planets', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('films');
+        const filmData = await collection.aggregate([
+            { $match: { id } },
+            {
+                $lookup: {
+                    from: 'films_planets',
+                    localField: 'id',
+                    foreignField: 'film_id',
+                    as: 'planetList',
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: 'planets',
+                                localField: 'planet_id',
+                                foreignField: 'id',
+                                as: 'planet'
+                            }
+                        },
+                        {
+                            $unwind: "$planet"
+                        }
+                    ]
+                }
+            }
+        ]).toArray();
+
+        const planets = filmData[0]?.planetList.map((item) => {
+            return item.planet;
+        });
+
+        if (!planets) res.json({ error: "No planets found" })
+        else res.json(planets);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
-app.get('/api/characters/:id/films', (req, res) => {
-    const id = req.params.id;
-    
+app.get('/api/characters/:id/films', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('characters');
+        const characterData = await collection.aggregate([
+            { $match: { id } },
+            {
+                $lookup: {
+                    from: 'films_characters',
+                    localField: 'id',
+                    foreignField: 'character_id',
+                    as: 'filmList',
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: 'films',
+                                localField: 'film_id',
+                                foreignField: 'id',
+                                as: 'film'
+                            }
+                        },
+                        {
+                            $unwind: "$film"
+                        }
+                    ]
+                }
+            }
+        ]).toArray();
+
+        const films = characterData[0]?.filmList.map((item) => {
+            return item.film;
+        });
+
+        if (!films) res.json({ error: "No films found" })
+        else res.json(films);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
-app.get('/api/planets/:id/films', (req, res) => {
-    const id = req.params.id;
-    
+app.get('/api/planets/:id/films', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('planets');
+        const planetData = await collection.aggregate([
+            { $match: { id } },
+            {
+                $lookup: {
+                    from: 'films_planets',
+                    localField: 'id',
+                    foreignField: 'planet_id',
+                    as: 'filmList',
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: 'films',
+                                localField: 'film_id',
+                                foreignField: 'id',
+                                as: 'film'
+                            }
+                        },
+                        {
+                            $unwind: "$film"
+                        }
+                    ]
+                }
+            }
+        ]).toArray();
+
+        const films = planetData[0]?.filmList.map((item) => {
+            return item.film;
+        });
+
+        if (!films) res.json({ error: "No films found" })
+        else res.json(films);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
-app.get('/api/planets/:id/characters', (req, res) => {
-    const id = req.params.id;
-    
+app.get('/api/planets/:id/characters', async (req, res) => {
+    const id = parseInt(req.params.id);
+    res.send('Not Complete');
 });
 
 
