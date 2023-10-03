02b87d3d2b5d962dcbd0e8d046c8cff264e513d7
Christian Smith <christian.46.smith@gmail.com>
2023-10-03 10:21:52 -0400

    Added routes to server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,60 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World');
-})
+});
+
+app.get('/api/characters', (req, res) => {
+
+});
+
+app.get('/api/films', (req, res) => {
+    
+});
+
+app.get('/api/planets', (req, res) => {
+    
+});
+
+app.get('/api/characters/:id', (req, res) => {
+    const id = req.params.id;
+});
+
+app.get('/api/films/:id', (req, res) => {
+    const id = req.params.id;
+    
+});
+
+app.get('/api/planets/:id', (req, res) => {
+    const id = req.params.id;
+    
+});
+
+app.get('/api/films/:id/characters', (req, res) => {
+    const id = req.params.id;
+    
+});
+
+app.get('/api/films/:id/planets', (req, res) => {
+    const id = req.params.id;
+    
+});
+
+app.get('/api/characters/:id/films', (req, res) => {
+    const id = req.params.id;
+    
+});
+
+app.get('/api/planets/:id/films', (req, res) => {
+    const id = req.params.id;
+    
+});
+
+app.get('/api/planets/:id/characters', (req, res) => {
+    const id = req.params.id;
+    
+});
+
+
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`)
